993c1a485fddb00845d324e42739365bf6abb0aa
Nur Khaleda Mohd Hilmi <nurkhaleda.mh@gmail.com>
2024-07-19 20:18:34 +0800

    initial commit

---
+++ Vocab.java
@@ -0,0 +1,22 @@
+
+public class Vocab {
+
+	String word;
+	String colour;
+	
+	public Vocab(String worded, String coloured)
+	{
+		this.word = worded;
+		this.colour = coloured;
+	}
+	
+	public String getWord()
+	{
+		return word;
+	}
+	
+	public String getColour()
+	{
+		return colour;
+	}
+}
